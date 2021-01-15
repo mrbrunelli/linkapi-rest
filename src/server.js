67b02d4cc7dd9eeb00be24607f453efbdb3eed63
Matheus R. Brunelli <matheus.brunelli@gmail.com>
2021-01-14 22:18:37 -0300

    feat: add error404 middleware in server

--- src/server.js
+++ src/server.js
@@ -1,11 +1,14 @@
 require("dotenv/config")
 const express = require("express")
+const routes = require("./routes")
+const {error404} = require("./middlewares")
 const app = express()
-
 const PORT = process.env.PORT || 3333
 
 app.use(express.json())
 app.use(express.urlencoded({extended: true}))
+app.use(routes)
+app.get("*", error404)
 
 app.listen(PORT, () => {
     console.log("Server running on port -> " + PORT)
